a7a87efd1d68f4d7ea5cc6c50d0471697b2c8fb7
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2024-02-14 22:45:32 +0100

    Prueba Rick

---
+++ assets/script.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+    var contador = 1; // Inicializar contador
+    var primeraParteResuelta = false; // Variable booleana para determinar si se ha resuelto la primera parte del puzzle
+
+    $('.rick').click(function() {
+        $('.popup').fadeIn();
+    });
+
+    $('#close').click(function() {
+        $('.popup').fadeOut();
+        contador = 1; // Reiniciar el contador al cerrar el popup
+    });
+
+    $('#next').click(function() {
+        // Aumentar contador
+        contador++;
+
+        // Mostrar mensaje dependiendo del contador y del valor de la variable primeraParteResuelta
+        switch (contador) {
+            case 1:
+                $('#message').text("¡Ja! ¿Crees que eres lo suficientemente inteligente para resolver estos enigmas? No me hagas reír.");
+                break;
+            case 2:
+                if (primeraParteResuelta) {
+                    $('#message').text("Hmm... Parece que estás luchando un poco. ¿Necesitas una pista, patético humano?");
+                } else {
+                    $('#message').text("¡Tú, patético humano! ¿Necesitas que te guíe paso a paso? Vaya decepción.");
+                }
+                break;
+            case 3:
+                $('#message').text("Hmm... parece que no eres más que un montón de carne inútil. ¡Intenta hacer algo útil por una vez en tu vida!");
+                break;
+            case 4:
+                $('#message').text("¡Qué sorpresa! ¡Sigues aquí! ¿Necesitas que te diga cómo respirar también?");
+                break;
+            case 5:
+                if (primeraParteResuelta) {
+                    $('#message').text("¡Bah! ¡Finalmente lo hiciste! Pero, ¿necesitas una pista? ¡Eres más patético de lo que pensaba!");
+                } else {
+                    $('#message').text("¡Esto es un desastre! ¡Ni siquiera puedes resolver la primera parte del puzzle! ¡Eres inútil!");
+                }
+                $('#next').hide(); // Ocultar el botón "Siguiente"
+                $('#close').show(); // Mostrar el botón "Cerrar"
+                break;
+            // Agregar más casos según sea necesario
+            default:
+                $('#message').text("¡Vaya, vaya! Parece que estás perdido. ¿Necesitas un manual para abrir una puerta?");
+                break;
+        }
+    });
+
+    $('#hoja').click(function() {
+        $('.popup-hoja').fadeIn();
+    });
+
+    $(document).click(function(event) {
+        if (!$(event.target).closest('.popup-hoja').length && !$(event.target).is('.hoja')) {
+            $('.popup-hoja').fadeOut();
+        }
+    });
+
+   
+        $('#caja').click(function() {
+            $('.panel').fadeIn();
+            $('.panel').css('display', 'flex'); // Cambiar el estilo CSS a display: flex;
+
+        });
+ 
+
+    var numbers = []; // Array para almacenar los números seleccionados
+
+    $('.digit').click(function() {
+        var number = $(this).text(); // Obtener el número del elemento clicado
+        numbers.push(number); // Añadir el número al array
+        $('.display').text(numbers.join('')); // Mostrar los números en el display
+    });
+
+    $('.trash').click(function() {
+        numbers = []; // Vaciar el array
+        $('.display').text(''); // Limpiar el display
+    });
+});
---
+++ index.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Prueba Rick</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+        <link rel="stylesheet" href="assets/style.css">
+      <script defer src="assets/script.js"></script>
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body>
+        
+    <img src="img/rick1.png" alt="" id="rick" class="rick">
+    <img src="img/hoja-garaje.png" alt="" id="hoja" class="hoja">
+    <img src="img/caja-fuerte.png" alt="" id="caja" class="caja">
+
+<div class="popup">
+    <p id="message">¡Oye tú! ¿Qué haces aquí? ¿No tienes nada mejor que hacer que estar atrapado en este absurdo escape room?</p>
+    <div class="buttons">
+        <button id="next">Siguiente</button>
+        <button id="close" style="display: none;">Cerrar</button>
+    </div>
+</div>
+
+<div class="popup-hoja" id="popup-hoja">
+            <p>--. .. .-.. .. .--. --- .-.. .-.. .- ... .-.-.</p>
+</div>
+<div class="panel">
+    <div class="display"></div> 
+    <div class="dial">
+        <div class="row">
+            <div class="digit">1</div>
+            <div class="digit">2</div>
+            <div class="digit">3</div>
+        </div>
+        <div class="row">
+            <div class="digit">4</div>
+            <div class="digit">5</div>
+            <div class="digit">6</div>
+        </div>
+        <div class="row">
+            <div class="digit">7</div>
+            <div class="digit">8</div>
+            <div class="digit">9</div>
+        </div>
+    </div>
+    <button class="accept"><i class="fas fa-check"></i> </button>
+    <div class="trash"><i class="fas fa-trash-alt"></i></div>
+    <div class="dial-top"><i class="fas fa-arrow-up"></i></div>
+</div>
+
+        
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
